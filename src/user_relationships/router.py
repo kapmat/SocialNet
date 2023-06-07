15b2960166842db6ce3056ca899fd6ee31476f51
kapmat <kapitonov.matvey@gmail.com>
2023-06-07 18:08:59 +0300

    Add exception

--- src/user_relationships/router.py
+++ src/user_relationships/router.py
@@ -1,9 +1,11 @@
+import sqlalchemy.exc
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy import select, insert, update, delete, inspect
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from auth.base_config import current_user
-from user_relationships.models import *
+from auth.models import User
+from user_relationships.models import Relationship as Rs
 from database import get_async_session
 
 
@@ -18,8 +20,7 @@ async def get_friend_list(
         user: User = Depends(current_user),
         session: AsyncSession = Depends(get_async_session)
 ):
-    query = select(Relationship.another_user_id)\
-        .where(Relationship.user_id == user.id, Relationship.status_id == 3)
+    query = select(Rs.another_user_id).where(Rs.user_id == user.id, Rs.status_id == 3)
     result = await session.execute(query)
     return result.scalars().all()
 
@@ -29,8 +30,7 @@ async def get_friend_out_requests(
         user: User = Depends(current_user),
         session: AsyncSession = Depends(get_async_session)
 ):
-    query = select(Relationship.another_user_id)\
-        .where(Relationship.user_id == user.id,  Relationship.status_id == 2)
+    query = select(Rs.another_user_id).where(Rs.user_id == user.id,  Rs.status_id == 2)
     result = await session.execute(query)
     return result.scalars().all()
 
@@ -40,8 +40,7 @@ async def get_friend_in_requests(
         user: User = Depends(current_user),
         session: AsyncSession = Depends(get_async_session)
 ):
-    query = select(Relationship.another_user_id)\
-        .where(Relationship.user_id == user.id,  Relationship.status_id == 1)
+    query = select(Rs.another_user_id).where(Rs.user_id == user.id,  Rs.status_id == 1)
     result = await session.execute(query)
     return result.scalars().all()
 
@@ -53,21 +52,34 @@ async def add_friend_request(
         session: AsyncSession = Depends(get_async_session)
 ):
     try:
-        # new_friend_id != user.id
-        stmt = insert(Relationship)\
-            .values(user_id=user.id, another_user_id=new_friend_id, status_id=2)
+        if new_friend_id == user.id:
+            raise HTTPException(status_code=400, detail="Недопустимое значение")
+
+        result = await session.execute(select(Rs).where(Rs.user_id == user.id, Rs.another_user_id == new_friend_id))
+
+        if len(result.all()):
+            raise HTTPException(status_code=400, detail="Заявка уже отправлена")
+
+        stmt = insert(Rs).values(user_id=user.id, another_user_id=new_friend_id, status_id=2)
         await session.execute(stmt)
-        stmt = insert(Relationship)\
-            .values(user_id=new_friend_id, another_user_id=user.id, status_id=1)
+        stmt = insert(Rs).values(user_id=new_friend_id, another_user_id=user.id, status_id=1)
         await session.execute(stmt)
         await session.commit()
+
         return {
-            "status": 201
+            "status_code": 201,
+            "detail": f"Пользователю {new_friend_id} отправлена заявка",
+            "headers": None
         }
+    except HTTPException as ex:
+        return ex
+
     except Exception:
-        raise HTTPException(status_code=500, detail={
-            "status": 500
-        })
+        return {
+            "status_code": 400,
+            "detail": f"Недопустимое значение",
+            "headers": None
+        }
 
 
 @router.patch("")
@@ -76,13 +88,25 @@ async def add_friend(
         user: User = Depends(current_user),
         session: AsyncSession = Depends(get_async_session)
 ):
-    stmt = update(Relationship)\
-        .values(status_id=3)\
-        .where(Relationship.user_id == user.id, Relationship.another_user_id == new_friend_id)
-    await session.execute(stmt)
-    stmt = update(Relationship)\
-        .values(status_id=3)\
-        .where(Relationship.user_id == new_friend_id, Relationship.another_user_id == user.id)
-    await session.execute(stmt)
-    await session.commit()
-    return {"status": 201}
+    try:
+        stmt = update(Rs)\
+            .values(status_id=3)\
+            .where(Rs.user_id == user.id, Rs.another_user_id == new_friend_id, Rs.status_id == 1)
+        result = await session.execute(stmt)
+
+        if not result.rowcount:
+            raise Exception
+
+        stmt = update(Rs)\
+            .values(status_id=3)\
+            .where(Rs.user_id == new_friend_id, Rs.another_user_id == user.id, Rs.status_id == 2)
+        await session.execute(stmt)
+        await session.commit()
+
+        return {
+            "status_code": 201,
+            "detail": f"Пользователь {new_friend_id} добавлен в друзья",
+            "headers": None
+        }
+    except Exception:
+        return HTTPException(status_code=400, detail="Недопустимое значение")
